c96d8ac4c54fcad5e119c47e92e4c51ecb6b005e
compasslg <lihanwei4c@gmail.com>
2017-11-19 17:31:33 -0700

    Canvas Added
    created a file game.php, including a html canvas and javascript functions to draw the game board

---
+++ game.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Sudoku</title>
+</head>
+<body onload="drawBoard()">
+<canvas id="gameScreen" width="576" height="576" style="background-color: black;"></canvas>
+<script>
+var boardSize = 9;
+var gridSize = 64;
+var gameScreen = document.getElementById("gameScreen");
+var context=gameScreen.getContext("2d");
+// Draw a boardSize x boardSize grid board on canvas 
+function drawBoard(){
+	var board = Array(boardSize);
+	for(var i=0; i < boardSize; i++){
+		board[i] = Array(boardSize);
+		if(i!=0){
+			if(i % 3 == 0){
+				context.strokeStyle="red";
+			}else{
+				context.strokeStyle="white";
+			}
+			drawLine(0, i*gridSize, gridSize * boardSize, i*gridSize);
+			drawLine(i*gridSize, 0, i*gridSize, gridSize * boardSize);
+		}
+		
+	}
+}
+function drawLine(x0, y0, x1, y1, width){
+	context.beginPath();
+	context.moveTo(x0, y0);
+	context.lineTo(x1, y1);
+	context.stroke();
+}
+</script>
+</body>
+</html>
